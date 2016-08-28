3c2550e79bcce9ee6ce0fe4af987b87cc57f9242
Adriana Zestroiu <adriana.zestroiu@yahoo.com>
2016-08-28 22:16:49 +0000

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ while (katzDeliLine.length>0) {
   return (`Currently serving ${personServed}.`);
 }
 }
-function currentLine(line){
+function currentLine(line){ var string=[];
 if (line.length==0) return ("The line is currently empty.");
 for (var i=0;i<line.length;i++) {
-return (`The line is currently: ${i+1} ${line[i]},`);
-}
+string.push(`${i + 1}. ${line[i]}`); }
+
+return (`The line is currently: ${string.join(', ')}`);
 }
