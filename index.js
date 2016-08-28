f102f916be6a0744d3cfeee5507737ffdb72f2da
Adriana Zestroiu <adriana.zestroiu@yahoo.com>
2016-08-28 22:05:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber(katzDeliLine,name){
+katzDeliLine.push(name);
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length==0) {return ("There is nobody waiting to be served!");}
+while (katzDeliLine.length>0) {
+  var personServed=katzDeliLine[0];
+  katzDeliLine.shift();
+  return (`Currently serving ${personServed}.`);
+}
+}
+function currentLine(line){
+if (line.length==0) return ("The line is currently empty.");
+for (var i=0;i<line.length;i++) {
+return (`The line is currently: ${i+1} ${line[i]},`);
+}
+}
